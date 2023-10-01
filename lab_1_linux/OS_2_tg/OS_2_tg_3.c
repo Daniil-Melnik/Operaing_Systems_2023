a958d89041713bfa19ba94b3e69e17ef66f102ea
DanilaMaster <daniil.melnik.03@bk.ru>
2023-10-01 15:32:23 +0300

    added 4 base versions of lab1 on linux

--- lab_1_linux/OS_2_tg/OS_2_tg_3.c
+++ lab_1_linux/OS_2_tg/OS_2_tg_3.c
@@ -1,1 +1,140 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <aio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
+
+#include <stdint.h>
+#include <inttypes.h>
+
+struct aio_operation {
+    struct aiocb aio;
+    char *buffer;
+    int write_operation;
+    void* next_operation;
+};
+
+void copy_file(const char* src_file_path, const char* dest_file_path, int num_operations, size_t cluster_size) {
+    int src_file = open(src_file_path, O_RDONLY);
+    if (src_file == -1) {
+        perror("Failed to open source file");
+        return;
+    }
+
+    int dest_file = open(dest_file_path, O_WRONLY | O_CREAT | O_TRUNC, 0666);
+    if (dest_file == -1) {
+        perror("Failed to open destination file");
+        close(src_file);
+        return;
+    }
+
+    struct stat src_file_stat;
+    if (fstat(src_file, &src_file_stat) == -1) {
+        perror("Failed to get source file size");
+        close(src_file);
+        close(dest_file);
+        return;
+    }
+
+    size_t src_file_size = src_file_stat.st_size;
+
+    // Calculate the number of clusters in the source file
+    size_t num_clusters = (src_file_size + cluster_size - 1) / cluster_size;
+
+    struct aio_operation* operations = malloc(sizeof(struct aio_operation) * num_operations);
+
+    off_t offset = 0;
+    for (size_t i = 0; i < num_clusters; i++) {
+        size_t remaining_bytes = src_file_size - offset;
+        size_t bytes_to_read = remaining_bytes < cluster_size ? remaining_bytes : cluster_size;
+
+        // Initialize the aio control block for the current operation
+        memset(&(operations[i].aio), 0, sizeof(struct aiocb));
+        operations[i].aio.aio_fildes = src_file;
+        operations[i].aio.aio_buf = operations[i].buffer;
+        operations[i].aio.aio_nbytes = bytes_to_read;
+        operations[i].aio.aio_offset = offset;
+
+        // Allocate buffer for the current operation
+        operations[i].buffer = malloc(bytes_to_read);
+
+        if (i != num_clusters - 1) {
+            operations[i].write_operation = 1; // Set as write operation for all operations except the last one
+        } else {
+            operations[i].write_operation = 0; // Set as read operation for the last operation
+        }
+
+        if (i != num_clusters - 1) {
+            operations[i].next_operation = &(operations[i + 1]); // Set the next operation as the next struct in the array
+        } else {
+            operations[i].next_operation = NULL; // Set the last operation's next_operation as NULL
+        }
+
+        // Start the aio operation
+        if (aio_read(&(operations[i].aio)) == -1) {
+            perror("Failed to start aio operation");
+            close(src_file);
+            close(dest_file);
+            free(operations);
+            return;
+        }
+
+        offset += cluster_size;
+    }
+
+    // Wait for the last operation to complete
+    while (aio_error(&(operations[num_clusters - 1].aio)) == EINPROGRESS) {
+        // Allow the calling thread to do some other work while waiting
+        usleep(100);
+    }
+
+    // Copy the read data to the destination file
+    struct aio_operation* current_operation = &(operations[0]);
+    while (current_operation != NULL) {
+        if (current_operation->write_operation) {
+            ssize_t bytes_written = aio_write(dest_file, current_operation->buffer, current_operation->aio.aio_nbytes);
+            if (bytes_written == -1) {
+                perror("Failed to write data to destination file");
+                break;
+            }
+
+            if (aio_error(&(current_operation->aio)) != 0) {
+                perror("Error occurred during aio operation");
+                break;
+            }
+
+            aio_return(&(current_operation->aio));
+            }
+
+        current_operation = current_operation->next_operation;
+    }
+
+    // Free resources
+    for (size_t i = 0; i < num_clusters; i++) {
+        free(operations[i].buffer);
+    }
+    free(operations);
+
+    // Close the files
+    close(src_file);
+    close(dest_file);
+}
+
+int main() {
+    const char* src_file_path = "source.txt";
+    const char* dest_file_path = "destination.txt";
+    int num_operations = 4;
+    size_t cluster_size = 4096;
+
+    copy_file(src_file_path, dest_file_path, num_operations, cluster_size);
+
+    return 0;
+}
--- lab_1_linux/OS_2_tg/OS_2_tg_4.c
+++ lab_1_linux/OS_2_tg/OS_2_tg_4.c
@@ -1,1 +1,134 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <aio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <errno.h>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 4096
+
+/*
+$ gcc -o aio_copy aio_copy.c
+$ ./aio_copy input.txt output.txt
+*/
+
+struct aio_operation {
+    struct aiocb aio;
+    char *buffer;
+    int write_operation;
+    void* next_operation;
+};
+
+void aio_completion_handler(int signo, siginfo_t *info, void *context) {
+    struct aio_operation* operation = (struct aio_operation*)info->si_value.sival_ptr;
+    off_t offset = aio_offset(&operation->aio);
+    ssize_t ret = aio_return(&operation->aio);
+
+    if (ret == -1) {
+        printf("AIO error: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    if (operation->write_operation) {
+        printf("Write operation completed at offset %lld\n", (long long)offset);
+    } else {
+        printf("Read operation completed at offset %lld\n", (long long)offset);
+    }
+
+    free(operation->buffer);
+    free(operation);
+}
+
+void create_aio_operation(struct aio_operation* operation, int fd_in, int fd_out, off_t offset, int size) {
+    memset(&operation->aio, 0, sizeof(struct aiocb));
+    operation->aio.aio_fildes = fd_in;
+    operation->aio.aio_offset = offset;
+    operation->aio.aio_buf = operation->buffer;
+    operation->aio.aio_nbytes = size;
+    operation->aio.aio_sigevent.sigev_notify = SIGEV_SIGNAL;
+    operation->aio.aio_sigevent.sigev_signo = SIGRTMIN;
+    operation->aio.aio_sigevent.sigev_value.sival_ptr = operation;
+
+    operation->write_operation = 1;
+    if (aio_read(&operation->aio) == -1) {
+        printf("AIO read error: %s\n", strerror(errno));
+        exit(1);
+    }
+
+    operation->aio.aio_fildes = fd_out;
+    operation->write_operation = 0;
+    if (aio_write(&operation->aio) == -1) {
+        printf("AIO write error: %s\n", strerror(errno));
+        exit(1);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <input_file> <output_file>\n", argv[0]);
+        return 1;
+    }
+
+    int fd_in = open(argv[1], O_RDONLY);
+    if (fd_in == -1) {
+        printf("Failed to open input file: %s\n", strerror(errno));
+        return 1;
+    }
+
+    struct stat statbuf;
+    if (fstat(fd_in, &statbuf) == -1) {
+        printf("Failed to get input file info: %s\n", strerror(errno));
+        return 1;
+    }
+
+    int fd_out = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, statbuf.st_mode);
+    if (fd_out == -1) {
+        printf("Failed to open output file: %s\n", strerror(errno));
+        return 1;
+    }
+
+    int cluster_size = statbuf.st_blksize;
+
+    struct aio_operation* operations = NULL;
+
+    off_t offset = 0;
+    while (offset < statbuf.st_size) {
+        struct aio_operation* operation = malloc(sizeof(struct aio_operation));
+
+        operation->buffer = malloc(cluster_size);
+        create_aio_operation(operation, fd_in, fd_out, offset, cluster_size);
+
+        operation->next_operation = operations;
+        operations = operation;
+
+        offset += cluster_size;
+    }
+
+    struct sigaction sa;
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = aio_completion_handler;
+    sigemptyset(&sa.sa_mask);
+
+    if (sigaction(SIGRTMIN, &sa, NULL) == -1) {
+        printf("Failed to set signal handler: %s\n", strerror(errno));
+        return 1;
+    }
+
+    while (operations != NULL) {
+        struct aio_operation* operation = operations;
+        operations = operation->next_operation;
+
+        while (aio_error(&operation->aio) == EINPROGRESS) {}
+    }
+
+    close(fd_in);
+    close(fd_out);
+
+    return 0;
+}

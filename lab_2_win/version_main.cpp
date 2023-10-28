3b67e7cb5d39ce3f6df3167286b184a9341b9a72
Daniil-Melnik <daniil.melnik.03@bk.ru>
2023-10-28 15:19:31 +0300

    added systemInfo

---
+++ lab_2_win/version_main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int showMenu() {
+    int k = 0;
+    cout << "1. Получить информацию о вычислительной системе" << std::endl;
+    cout << "0. Выход" << std::endl;
+    cout << "Выберите опцию: ";
+    cin >> k;
+
+    return k;
+}
+
+void displaySystemInfo() {
+    SYSTEM_INFO systemInfo;
+
+    GetSystemInfo(&systemInfo);
+
+    cout << "Информация о вычислительной системе:" << endl;
+    cout << "Архитектура процессора: " << systemInfo.dwProcessorType << endl;
+    /*
+    * PROCESSOR_INTEL_386 (386)
+    * PROCESSOR_INTEL_486 (486)
+    * PROCESSOR_INTEL_PENTIUM (586)
+    * PROCESSOR_INTEL_IA64 (2200)
+    * PROCESSOR_AMD_X8664 (8664)
+    * PROCESSOR_ARM (зарезервировано)
+    */
+
+    cout << "Архитектура процессора операционной системы: " << systemInfo.wProcessorArchitecture << endl;
+    cout << "Количество процессоров: " << systemInfo.dwNumberOfProcessors << endl;
+    cout << "Размер страницы памяти: " << systemInfo.dwPageSize << endl;
+    cout << "Cамый низкий адрес для динамической компоновки (DLL): " << systemInfo.lpMinimumApplicationAddress << endl;
+    cout << "Cамый высокий адрес для динамической компоновки (DLL): " << systemInfo.lpMaximumApplicationAddress << endl;
+    cout << "Активные процессоры в системе: " << systemInfo.dwActiveProcessorMask << endl;
+    cout << "Степень детализации для начального адреса: " << systemInfo.dwAllocationGranularity << endl;
+    cout << "Уровень процессора (1 - IA64, другой - INTEL): " << systemInfo.wProcessorLevel << endl;
+    cout << "Редакция процессора: " << systemInfo.wProcessorRevision << endl;
+    /*
+    * Intel Pentium, Cyrix или NextGen 586:
+    * -- xxyy = Model xx, Stepping yy
+    *
+    * Intel 80386 или 80486
+    * -- xxyz
+    * Если xx равно 0xFF, y - 0xA — номер модели, а z — идентификатор шага
+    * Если xx не равно 0xFF, xx + "A" — это буква шага, а yz — это незначительный шаг.
+    *
+    * -- ARM - зарезервировано
+    */
+}
+
+int main()
+{
+    SetConsoleCP(1251); // Ввод с консоли в кодировке 1251
+    SetConsoleOutputCP(1251);
+
+    int option = 0;
+
+    while (1)
+    {
+        option = showMenu();
+
+        cout << endl << "Выбранная опция: " << option << endl;
+
+        switch(option){
+        case 1:
+            displaySystemInfo();
+            break;
+        case 0:
+            return 0;
+        }
+    }
+}
